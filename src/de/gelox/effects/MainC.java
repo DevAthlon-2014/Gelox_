396b6fe64ad8c46e888f5e1f5a9d6dda4e449bb5
Marcosio <MBoerner00@gmx.de>
2014-10-18 10:39:56 +0200

    Test

---
+++ src/de/gelox/effects/MainC.java
@@ -0,0 +1,17 @@
+package de.gelox.effects;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Marco_2 on 18.10.2014.
+ */
+public class MainC extends JavaPlugin {
+
+    public void onEnable() {
+        loadCmd();
+    }
+
+    public void loadCmd() {
+        //TODO
+    }
+}
