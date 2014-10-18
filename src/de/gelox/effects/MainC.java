f7c0acc09f91898844cbfc4cec995c9e63e55cf2
Marcosio <MBoerner00@gmx.de>
2014-10-18 11:31:26 +0200

    Added some things

--- src/de/gelox/effects/MainC.java
+++ src/de/gelox/effects/MainC.java
@@ -1,5 +1,6 @@
 package de.gelox.effects;
 
+import de.gelox.effects.effects.superthunder;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,12 +13,22 @@ public class MainC extends JavaPlugin {
     @Getter
     @Setter
 
+    private static MainC instance;
+
     public String prefix = "§7[§6Effects§7] §3";
 
     public void onEnable() {
+        instance = this;
         loadCmd();
         loadEvents();
         loadCfg();
+
+
+
+    }
+
+    public void onDisable() {
+        instance = null;
     }
 
     public void loadCmd() {
@@ -27,5 +38,13 @@ public class MainC extends JavaPlugin {
     public void loadCfg() {
         getConfig().options().copyDefaults(true);
         saveConfig();
+    }
+
+    public void loadEvents() {
+        getServer().getPluginManager().registerEvents(new superthunder(getInstance()), this);
+    }
+
+    public static MainC getInstance() {
+        return instance;
     }
 }
--- src/de/gelox/effects/effects/superthunder.java
+++ src/de/gelox/effects/effects/superthunder.java
@@ -3,6 +3,7 @@ package de.gelox.effects.effects;
 import de.gelox.effects.MainC;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -25,18 +26,53 @@ public class superthunder implements Listener, CommandExecutor {
 
 
     public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
+
+        if(!(args.length == 1)) {
+            cs.sendMessage(plugin.getPrefix() + "/superthunder");
+        }
         return true;
     }
 
     @EventHandler
     public void onMove(PlayerMoveEvent e) {
 
-        Player p = e.getPlayer();
-        Location loc = p.getLocation();
+        if(plugin.getConfig().getBoolean("superthunder")) {
 
-        String w = p.getWorld().getName();
+            Player p = e.getPlayer();
+            Location loc = p.getLocation();
+
+            String w = loc.getWorld().getName();
+
+            Bukkit.getWorld(w).strikeLightningEffect(loc);
+
+            Bukkit.getWorld(w).setTime(16000);
 
 
+            Location location = new Location(p.getWorld(), 0, 0, 0); // New Location in the right World you want
 
+            location.setX( location.getX() + Math.random() * 100 * 2 - 534); // This get a Random with a MaxRange
+            location.setZ( location.getZ() + Math.random() * 323 * 2 - 234);
+
+            location.setY( p.getWorld().getHighestBlockAt(location.getBlockX(), location.getBlockZ() ).getY() ); // Get the Highest Block of the Location for Save Spawn.
+
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
